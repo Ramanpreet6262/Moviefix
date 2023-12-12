77fda423010fef2e67de4e0159246710ee176343
Ramanpreet6262 <ramanpreet.singh@squadstack.com>
2023-12-12 09:16:18 +0530

    fix: Fixed typo in file name

--- services/network/index.ts
+++ services/network/index.ts
@@ -1,4 +1,4 @@
 import {useQuery, useMutation} from '@tanstack/react-query';
-import {QueryStore} from './queryStore';
+import {QueryStore} from './QueryStore';
 
 export {QueryStore, useQuery, useMutation};
