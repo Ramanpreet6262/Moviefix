d4342d74cce2b8c4333c94eddcbd8ca9cb46dc64
Ramanpreet6262 <ramanpreet.singh@squadstack.com>
2023-12-25 03:53:25 +0530

    fix: removed unnecessary code from app

--- App.tsx
+++ App.tsx
@@ -1,54 +1,18 @@
 import React from 'react';
-// import type {PropsWithChildren} from 'react';
-import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {SafeAreaView, StatusBar} from 'react-native';
 import {QueryStore} from './services/network';
-
-// type SectionProps = PropsWithChildren<{
-//   title: string;
-// }>;
-
-// function Section({children, title}: SectionProps): React.JSX.Element {
-//   const isDarkMode = useColorScheme() === 'dark';
-//   return (
-//     <View style={styles.sectionContainer}>
-//       <Text
-//         style={[
-//           styles.sectionTitle,
-//           {
-//             color: isDarkMode ? Colors.white : Colors.black,
-//           },
-//         ]}>
-//         {title}
-//       </Text>
-//       <Text
-//         style={[
-//           styles.sectionDescription,
-//           {
-//             color: isDarkMode ? Colors.light : Colors.dark,
-//           },
-//         ]}>
-//         {children}
-//       </Text>
-//     </View>
-//   );
-// }
+import {ThemeProvider, theme} from './theme';
+import {HomeScreen} from './src/screens/HomeScreen';
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
     <QueryStore>
-      <SafeAreaView style={backgroundStyle}>
-        <StatusBar
-          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-          backgroundColor={backgroundStyle.backgroundColor}
-        />
-      </SafeAreaView>
+      <ThemeProvider theme={theme}>
+        <SafeAreaView style={{backgroundColor: 'black'}}>
+          <StatusBar barStyle={'light-content'} backgroundColor={'black'} />
+        </SafeAreaView>
+        <HomeScreen />
+      </ThemeProvider>
     </QueryStore>
   );
 }
